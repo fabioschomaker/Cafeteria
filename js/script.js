e33d865936762081519be29f1b59250e773bfb28
Fabio Schomaker Paranhos <132515511+fabioschomaker@users.noreply.github.com>
2023-08-26 17:22:05 -0300

    Atualizado animação

--- js/script.js
+++ js/script.js
@@ -15,3 +15,22 @@ window.onscroll = () =>{
     
 }
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '85px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal ('.home-texto',{delay:300});
+
+sr.reveal ('.sobre-img',{});
+sr.reveal ('.btn, .sobre-texto',{delay:400});
+
+sr.reveal ('.center-texto',{});
+sr.reveal (' .btn, .box-produtos',{delay:400});
+
+sr.reveal ('.box-clientes, .conainer-contato',{delay:400});
+
+
+
